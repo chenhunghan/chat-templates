be61a33ddf6766b495a4404cc69534b79aea6e4f
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2024-04-15 14:33:20 +0300

    feat: add support for taide
    Signed-off-by: Hung-Han (Henry) Chen <chenhungh@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,10 @@ const MISTRAL_INSTRUCT_TEMPLATE: &str = "{{ bos_token }}{% for message in messag
 
 const MISTRAL_INSTRUCT_TEMPLATE_NAME: &str = "mistral-instruct";
 
+const TAIDE_JINJA_TEMPLATE_NAME: &str = "taide";
+
+const TAIDE_JINJA_TEMPLATE: &str = "{% if messages[0]['role'] == 'system' %}{% set loop_messages = messages[1:] %}{% set system_message = '<<SYS>>\n' + messages[0]['content'] + '\n<</SYS>>\n\n' %}{% else %}{% set loop_messages = messages %}{% set system_message = '' %}{% endif %}{% for message in loop_messages %}{% if loop.index0 == 0 %}{% set content = system_message + message['content'] %}{% else %}{% set content = message['content'] %}{% endif %}{% if message['role'] == 'user' %}{{ bos_token + '[INST] ' + content + ' [/INST]'}}{% elif message['role'] == 'assistant' %}{{ ' '  + content + ' ' + eos_token }}{% endif %}{% endfor %}{% if add_generation_prompt %}{{ '<|im_start|>assistant\n' }}{% endif %}";
+
 /// Apply Chat Markup Language (chatml) template to messages, return the prompt
 fn apply_chatml_template(
     messages: &Vec<Message>,
@@ -59,10 +63,30 @@ fn apply_mistral_instruct_template(
         .map_err(ApplyMistralInstructTemplateError::RenderTemplateError)
 }
 
+/// Apply TAIDE template to messages, return the prompt
+fn apply_taide_template(
+  messages: &Vec<Message>,
+) -> Result<String, ApplyTAIDETemplateError> {
+  let mut env = Environment::new();
+  env.add_template(TAIDE_JINJA_TEMPLATE_NAME, TAIDE_JINJA_TEMPLATE)
+      .map_err(ApplyTAIDETemplateError::AddTemplateError)?;
+  let template = env
+      .get_template(TAIDE_JINJA_TEMPLATE_NAME)
+      .map_err(ApplyTAIDETemplateError::GetTemplateError)?;
+  template
+      .render(context! {
+        messages => messages,
+        bos_token => "<s>",
+        eos_token => "</s>",
+      })
+      .map_err(ApplyTAIDETemplateError::RenderTemplateError)
+}
+
 /// All available templates
 pub enum ChatTemplate {
     ChatML,
     MistralInstruct,
+    TAIDE
 }
 
 /// Apply chat template to messages, return the prompt
@@ -84,6 +108,8 @@ pub fn apply_template(
             apply_mistral_instruct_template(messages, add_generation_prompt)
                 .map_err(ApplyTemplateError::ApplyMistralInstructTemplateError)
         }
+        ChatTemplate::TAIDE => apply_taide_template(messages)
+            .map_err(ApplyTemplateError::ApplyTAIDETemplateError),
     }
 }
 
@@ -108,11 +134,24 @@ pub enum ApplyMistralInstructTemplateError {
 }
 
 #[derive(thiserror::Error, Debug)]
+pub enum ApplyTAIDETemplateError {
+    #[error("failed to add template")]
+    AddTemplateError(#[source] minijinja::Error),
+    #[error("failed to get template")]
+    GetTemplateError(#[source] minijinja::Error),
+    #[error("failed to render")]
+    RenderTemplateError(#[source] minijinja::Error),
+}
+
+
+#[derive(thiserror::Error, Debug)]
 pub enum ApplyTemplateError {
     #[error("failed to apply chatml template")]
     ApplyChatMLTemplateError(#[source] ApplyChatMLTemplateError),
     #[error("failed to apply mistral instruct template")]
     ApplyMistralInstructTemplateError(#[source] ApplyMistralInstructTemplateError),
+    #[error("failed to apply taide template")]
+    ApplyTAIDETemplateError(#[source] ApplyTAIDETemplateError),
 }
 
 #[cfg(test)]
@@ -208,5 +247,109 @@ mod tests {
 
         let prompt = apply_template(ChatTemplate::MistralInstruct, &messages, true).unwrap();
         assert_eq!(prompt, "<s>[INST] Hello, who are you? [/INST]I'm doing great. How can I help you today?</s>[INST] I'd like to show off how chat templating works! [/INST]Are you sure?</s>[INST] Yes! [/INST]".to_string());
+    }
+
+    #[test]
+    fn test_apply_taide_template_one_shot() {
+        let messages = vec![
+          Message {
+            role: "user".to_string(),
+            content: "你好嗎？".to_string(),
+          }
+        ];
+
+        // taide-chat template does not support add_generation_prompt = true
+        let prompt = apply_template(ChatTemplate::TAIDE, &messages, true).unwrap();
+        assert_eq!(prompt, "<s>[INST] 你好嗎？ [/INST]".to_string());
+        
+        let prompt = apply_template(ChatTemplate::TAIDE, &messages, false).unwrap();
+        assert_eq!(prompt, "<s>[INST] 你好嗎？ [/INST]".to_string());
+    }
+
+    #[test]
+    fn test_apply_taide_template_one_shot_with_sys_prompt() {
+        let messages = vec![
+          Message {
+            role: "system".to_string(),
+            content: "你是一個來自台灣的AI助理，你的名字是 TAIDE。".to_string(),
+          },
+          Message {
+            role: "user".to_string(),
+            content: "你好嗎？".to_string(),
+          }
+        ];
+
+        // taide-chat template does not support add_generation_prompt = true
+        let prompt = apply_template(ChatTemplate::TAIDE, &messages, true).unwrap();
+        assert_eq!(prompt, "<s>[INST] <<SYS>>\n你是一個來自台灣的AI助理，你的名字是 TAIDE。\n<</SYS>>\n\n你好嗎？ [/INST]".to_string());
+        
+        let prompt = apply_template(ChatTemplate::TAIDE, &messages, false).unwrap();
+        assert_eq!(prompt, "<s>[INST] <<SYS>>\n你是一個來自台灣的AI助理，你的名字是 TAIDE。\n<</SYS>>\n\n你好嗎？ [/INST]".to_string());
+    }
+
+    #[test]
+    fn test_apply_taide_template_few_shot_with_sys_prompt() {
+        let messages = vec![
+          Message {
+            role: "system".to_string(),
+            content: "你是一個來自台灣的AI助理，你的名字是 TAIDE。".to_string(),
+          },
+          Message {
+            role: "user".to_string(),
+            content: "你好嗎？".to_string(),
+          },
+          Message {
+            role: "assistant".to_string(),
+            content: "我很好。".to_string(),
+          },
+          Message {
+            role: "user".to_string(),
+            content: "今天天氣怎樣？".to_string(),
+          },
+        ];
+
+        // taide-chat template does not support add_generation_prompt = true
+        let prompt = apply_template(ChatTemplate::TAIDE, &messages, true).unwrap();
+        assert_eq!(prompt, "<s>[INST] <<SYS>>\n你是一個來自台灣的AI助理，你的名字是 TAIDE。\n<</SYS>>\n\n你好嗎？ [/INST] 我很好。 </s><s>[INST] 今天天氣怎樣？ [/INST]".to_string());
+        
+        let prompt = apply_template(ChatTemplate::TAIDE, &messages, false).unwrap();
+        assert_eq!(prompt, "<s>[INST] <<SYS>>\n你是一個來自台灣的AI助理，你的名字是 TAIDE。\n<</SYS>>\n\n你好嗎？ [/INST] 我很好。 </s><s>[INST] 今天天氣怎樣？ [/INST]".to_string());
+    }
+
+    #[test]
+    fn test_apply_taide_template_few_shot_conversation_sys_prompt() {
+        let messages = vec![
+          Message {
+            role: "system".to_string(),
+            content: "你是一個來自台灣的AI助理，你的名字是 TAIDE。".to_string(),
+          },
+          Message {
+            role: "user".to_string(),
+            content: "你好嗎？".to_string(),
+          },
+          Message {
+            role: "assistant".to_string(),
+            content: "我很好。".to_string(),
+          },
+          Message {
+            role: "user".to_string(),
+            content: "今天天氣怎樣？".to_string(),
+          },
+          Message {
+            role: "assistant".to_string(),
+            content: "大太陽。".to_string(),
+          },
+          Message {
+            role: "user".to_string(),
+            content: "你敢感覺如何？".to_string(),
+          },
+        ];
+
+        // taide-chat template does not support add_generation_prompt = true
+        let prompt = apply_template(ChatTemplate::TAIDE, &messages, true).unwrap();
+        assert_eq!(prompt, "<s>[INST] <<SYS>>\n你是一個來自台灣的AI助理，你的名字是 TAIDE。\n<</SYS>>\n\n你好嗎？ [/INST] 我很好。 </s><s>[INST] 今天天氣怎樣？ [/INST] 大太陽。 </s><s>[INST] 你敢感覺如何？ [/INST]".to_string());
+        
+        let prompt = apply_template(ChatTemplate::TAIDE, &messages, false).unwrap();
+        assert_eq!(prompt, "<s>[INST] <<SYS>>\n你是一個來自台灣的AI助理，你的名字是 TAIDE。\n<</SYS>>\n\n你好嗎？ [/INST] 我很好。 </s><s>[INST] 今天天氣怎樣？ [/INST] 大太陽。 </s><s>[INST] 你敢感覺如何？ [/INST]".to_string());
     }
 }
